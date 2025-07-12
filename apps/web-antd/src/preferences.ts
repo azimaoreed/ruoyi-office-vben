2ecbf37c6c11dbec75c73ffc3f02c74fef567a85
289957361@qq.com <289957361@qq.com>
2025-07-12 14:49:30 +0800

    feat: 设置默认布局和主题

--- apps/web-antd/src/preferences.ts
+++ apps/web-antd/src/preferences.ts
@@ -12,6 +12,10 @@ export const overridesPreferences = defineOverridesPreferences({
     accessMode: 'backend',
     name: import.meta.env.VITE_APP_TITLE,
     enableRefreshToken: true,
+    layout: 'mixed-nav',
+  },
+  theme: {
+    mode: 'light',
   },
   footer: {
     /** 默认关闭 footer 页脚，因为有一定遮挡 */
@@ -23,3 +27,4 @@ export const overridesPreferences = defineOverridesPreferences({
     companySiteLink: 'https://gitee.com/yudaocode/yudao-ui-admin-vben',
   },
 });
+
