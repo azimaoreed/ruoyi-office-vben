f759d68a0459b26c6265e4d49249e8cf99f1d668
289957361@qq.com <289957361@qq.com>
2025-07-13 21:13:18 +0800

    feat: 调整请求响应时间为60s

--- apps/web-antd/src/api/request.ts
+++ apps/web-antd/src/api/request.ts
@@ -130,6 +130,7 @@ function createRequestClient(baseURL: string, options?: RequestClientOptions) {
 
 export const requestClient = createRequestClient(apiURL, {
   responseReturn: 'data',
+  timeout: 60_000,
 });
 
 export const baseRequestClient = new RequestClient({ baseURL: apiURL });
